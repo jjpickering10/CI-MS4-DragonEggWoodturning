04fd7c90de18cdad9d43ef1882f0ba2922e23973
jjpickering10 <jjpickering10@live.com>
2021-11-06 17:15:32 +0000

    Remove bootstrap and add materialize. Set base navbar template

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
