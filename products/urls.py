a06a173d20e6122dd427b603d9023f27865bdafc
jjpickering10 <jjpickering10@live.com>
2021-11-07 19:35:42 +0000

    Set up initial products views and templates

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.all_products, name='all_products'),
     path('all_categories/', views.all_categories, name='all_categories'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Product
 
+
+def all_products(request):
+    """
+    A view to show all products
+    """
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/all_products.html', context)
 
 def all_categories(request):
     """
     A view to show all categories
     """
-
     categories = Category.objects.all()
 
     context = {
         'categories': categories,
     }
-    
+
     return render(request, 'products/all_categories.html', context)
