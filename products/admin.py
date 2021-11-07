9c194a3fbe28a41ee6111b0f28cf1286aa2a7d33
jjpickering10 <jjpickering10@live.com>
2021-11-07 15:47:47 +0000

    Customise admin

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,31 @@ from django.contrib import admin
 from .models import Product, Category, WoodType
 
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(WoodType)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'wood_type',
+        'sku',
+        'name',
+        'price',
+        'rating',
+        'image',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+class WoodTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(WoodType, WoodTypeAdmin)
