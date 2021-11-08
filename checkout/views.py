d2ae114ad964d539fffdea1ea940fe29e0cd78a4
jjpickering10 <jjpickering10@live.com>
2021-11-08 19:28:57 +0000

    Add checkout view to checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from .forms import OrderForm
 
-# Create your views here.
+
+def checkout(request):
+    """
+    A view to checkout
+    """
+
+    bag = request.session.get('bag', {})
+
+    if not bag:
+        return redirect(reverse('all_products'))
+
+    order_form = OrderForm()
+
+    template = 'checkout/checkout.html'
+
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
