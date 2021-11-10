0763bea6afd410781c1e7a651d675e948da1fd5d
jjpickering10 <jjpickering10@live.com>
2021-11-10 11:02:38 +0000

    Add JQuery, cache checkout data in payment intent and test

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -58,60 +58,74 @@ form.addEventListener('submit', function(ev) {
   submitButton.setAttribute('disabled', true)
   // If the client secret was rendered server-side as a data-secret attribute
   // on the <form> element, you can retrieve it here by calling `form.dataset.secret`
-  stripe.confirmCardPayment(clientSecret, {
-    payment_method: {
-      card: card,
-      billing_details: {
-        name: form.full_name.value,
-        phone: form.phone_number.value,
-        email: form.email.value,
+
+  var csrfToken = form.csrfmiddlewaretoken.value
+
+  var postData = {
+    'csrfmiddlewaretoken': csrfToken,
+    'client_secret': clientSecret,
+  }
+
+  var url = '/checkout/cache_checkout_data/'
+
+  $.post(url, postData).done(function(){
+    stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: card,
+        billing_details: {
+          name: form.full_name.value.trim(),
+          phone: form.phone_number.value.trim(),
+          email: form.email.value.trim(),
+          address: {
+            line1: form.street_address1.value.trim(),
+            line2: form.street_address2.value.trim(),
+            city: form.town_or_city.value.trim(),
+            country: form.country.value.trim(),
+            state: form.county.value.trim(),
+          }
+        }
+      },
+      shipping: {
+        name: form.full_name.value.trim(),
+        phone: form.phone_number.value.trim(),
         address: {
-          line1: form.street_address1.value,
-          line2: form.street_address2.value,
-          city: form.town_or_city.value,
-          country: form.country.value,
-          state: form.county.value,
+          line1: form.street_address1.value.trim(),
+          line2: form.street_address2.value.trim(),
+          city: form.town_or_city.value.trim(),
+          country: form.country.value.trim(),
+          postal_code: form.postcode.value.trim(),
+          state: form.county.value.trim(),
         }
       }
-    },
-    shipping: {
-      name: form.full_name.value,
-      phone: form.phone_number.value,
-      address: {
-        line1: form.street_address1.value,
-        line2: form.street_address2.value,
-        city: form.town_or_city.value,
-        country: form.country.value,
-        postal_code: form.postcode.value,
-        state: form.county.value,
+    }).then(function(result) {
+      const errorDiv = document.getElementById('card-errors')
+      if (result.error) {
+          const html = `
+          <span>
+              <i class="fas fa-times"></i>
+          </span>
+          <span>
+              ${result.error.message}
+          </span>
+      `
+          errorDiv.innerHTML = html
+          card.update({
+              'disabled': false
+          })
+          submitButton.removeAttribute('disabled')
+      } else {
+        // The payment has been processed!
+        if (result.paymentIntent.status === 'succeeded') {
+          // Show a success message to your customer
+          // There's a risk of the customer closing the window before callback
+          // execution. Set up a webhook or plugin to listen for the
+          // payment_intent.succeeded event that handles any business critical
+          // post-payment actions.
+          form.submit()
+        }
       }
-    }
-  }).then(function(result) {
-    const errorDiv = document.getElementById('card-errors')
-    if (result.error) {
-        const html = `
-        <span>
-            <i class="fas fa-times"></i>
-        </span>
-        <span>
-            ${result.error.message}
-        </span>
-    `
-        errorDiv.innerHTML = html
-        card.update({
-            'disabled': false
-        })
-        submitButton.removeAttribute('disabled')
-    } else {
-      // The payment has been processed!
-      if (result.paymentIntent.status === 'succeeded') {
-        // Show a success message to your customer
-        // There's a risk of the customer closing the window before callback
-        // execution. Set up a webhook or plugin to listen for the
-        // payment_intent.succeeded event that handles any business critical
-        // post-payment actions.
-        form.submit()
-      }
-    }
-  });
-});
+    });
+  }).fail(function(){
+    location.reload()
+  })
+})
--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.conf import settings
 from .forms import OrderForm
 from .models import OrderLineItem, Order
 from products.models import Product
 from bag.contexts import bag_contents
 import stripe
+import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        payment_id = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(payment_id, metadata={
+            'username': request.user,
+            'bag': json.dumps(request.session.get('bag')),
+        })
+
+        return HttpResponse(status=200)
+    except Exception as e:
+        print("Payment failed")
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -78,7 +96,6 @@ def checkout_success(request, order_number):
     """
 
     order = get_object_or_404(Order, order_number=order_number)
-    print(order.full_name)
 
     if 'bag' in request.session:
         del request.session['bag']
