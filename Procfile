e40895229c8316549d998cfc027d17d69eb0a932
jjpickering10 <jjpickering10@live.com>
2021-11-11 22:11:10 +0000

    Add Procfile and heroku host

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dragoneggwoodturning.wsgi:application
--- dragoneggwoodturning/settings.py
+++ dragoneggwoodturning/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.environ.get('HEROKU_HOSTNAME'),
+    'localhost'
+]
 
 
 # Application definition
