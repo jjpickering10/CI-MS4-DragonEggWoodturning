fce5809ef26db66d2c4d781fb31b77aac55fc0ff
jjpickering10 <jjpickering10@live.com>
2021-11-11 23:12:03 +0000

    Change to debug to false for production

--- dragoneggwoodturning/settings.py
+++ dragoneggwoodturning/settings.py
@@ -28,7 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if 'DEVELOPMENT' in os.environ:
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = [
     os.environ.get('HEROKU_HOSTNAME'),
