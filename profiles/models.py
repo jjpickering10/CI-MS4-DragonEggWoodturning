47f840232b39b9deda133d97baf9a1680a9dd65f
jjpickering10 <jjpickering10@live.com>
2021-11-12 18:11:16 +0000

    Add signals to save user to user profile

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django_countries.fields import CountryField
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class UserProfile(models.Model):
     """
@@ -20,3 +23,15 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create/update user profile
+    """
+
+    if created:
+        UserProfile.objects.create(user=instance)
+
+    instance.userprofile.save()
