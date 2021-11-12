e9db0425f4c3a3120ae3c1559c230e2aa82ddbd2
jjpickering10 <jjpickering10@live.com>
2021-11-12 19:22:55 +0000

    Add auth decorator login_required to profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+from .models import UserProfile
 
 
+@login_required
 def profile(request):
     """
     Display profile of user
     """
 
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profile.html'
 
     context = {
-
+        'profile': profile
     }
 
     return render(request, template, context)
