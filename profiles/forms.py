9d40f9f4cbb4e7a7ae36684b4daaa59381998924
jjpickering10 <jjpickering10@live.com>
2021-11-16 11:28:14 +0000

    Add forms.py to profiles app and show profile info in profile page

---
+++ profiles/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+        fields = (
+            'full_name',
+            'email',
+            'phone_number',
+            'street_address1',
+            'street_address2',
+            'town_or_city',
+            'county',
+            'postcode',
+            'country',
+        )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 @login_required
@@ -12,11 +13,14 @@ def profile(request):
     profile = UserProfile.objects.get(user=request.user)
     orders = profile.order_set.all()
 
+    form = UserProfileForm(instance=profile)
+
     template = 'profiles/profile.html'
 
     context = {
         'profile': profile,
         'orders': orders,
+        'form': form,
     }
 
     return render(request, template, context)
