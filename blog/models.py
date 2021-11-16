f9a0d296e913393d12cd4991b40059f362a5709c
jjpickering10 <jjpickering10@live.com>
2021-11-16 13:47:23 +0000

    Create blog models for post and comment

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    Blog model
+    """
+    title = models.CharField(max_length=100, unique=True)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    """
+    Comment model
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return self.post + ' - ' + self.user
