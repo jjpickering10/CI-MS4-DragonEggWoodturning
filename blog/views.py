3bd5a6732014aac1b2ba34cfe823c384c7ccd49d
jjpickering10 <jjpickering10@live.com>
2021-11-16 14:42:41 +0000

    Add blog data to html and style blog page layout

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-
+from .models import Post
 
 def blog(request):
     """
     Display blog page
     """
-
+    posts = Post.objects.all()
     template = 'blog/blog.html'
 
     context = {
-
+        'posts': posts
     }
 
     return render(request, template, context)
