3dda8209bd4020311b612f8c4aab247a8f1ae2df
jjpickering10 <jjpickering10@live.com>
2021-11-16 18:24:43 +0000

    Add post comment functionality

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .models import Post
+from .forms import CommentForm
 
 def blog(request):
     """
@@ -23,10 +24,24 @@ def blog_post(request, blog_id):
     """
     post = Post.objects.get(id=blog_id)
     print(post)
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.post = post
+            new_comment.save()
+            return redirect(reverse('blog_post', args=[blog_id]))
+        else:
+            return redirect(reverse('blog_post', args=[blog_id]))
+
+    comment_form = CommentForm()
     template = 'blog/blog_post.html'
 
     context = {
-        'post': post
+        'post': post,
+        'comment_form': comment_form
     }
 
     return render(request, template, context)
