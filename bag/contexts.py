f0d117326f7cffdd8758dcd8df120e31bfc719f2
jjpickering10 <jjpickering10@live.com>
2021-11-19 15:29:22 +0000

    Fix: change product price logic details to show discounted prices in bag and checkout

--- bag/contexts.py
+++ bag/contexts.py
@@ -13,7 +13,10 @@ def bag_contents(request):
 
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
+        if product.final_price:
+            total += quantity * product.final_price
+        else:
+            total += quantity * product.price
         product_count += quantity
         bag_items.append({
             'item_id': item_id,
--- checkout/views.py
+++ checkout/views.py
@@ -76,6 +76,7 @@ def checkout(request):
 
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
+        print(total)
         stripe_total = round(total * 100)
 
         stripe.api_key = stripe_secret_key
