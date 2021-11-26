43b9a60f250f6872c08413145e7ca2ad61964e82
jjpickering10 <jjpickering10@live.com>
2021-11-26 17:54:54 +0000

    Add delete product functionality

--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', views.all_products, name='all_products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('all_categories/', views.all_categories, name='all_categories'),
-    path('add_discount/<int:product_id>', views.add_discount, name='add_discount'),
-    path('category_discount/<int:category_id>', views.category_discount, name='category_discount'),
+    path('add_discount/<int:product_id>/', views.add_discount, name='add_discount'),
+    path('category_discount/<int:category_id>/', views.category_discount, name='category_discount'),
     path('add_product/', views.add_product, name='add_product'),
+    path('delete_product/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -199,6 +199,18 @@ def add_product(request):
         return redirect(reverse('profile'))
 
 
+def delete_product(request, product_id):
+    """
+    A view to delete a product
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('profile'))
 
 def all_categories(request):
     """
