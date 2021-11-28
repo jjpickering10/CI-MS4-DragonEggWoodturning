af2a9fa6babc52eaca25cee042394483c4c196f0
jjpickering10 <jjpickering10@live.com>
2021-11-28 14:53:24 +0000

    Add mousemove event to gallery images

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,24 @@
+// Materialize JS
+
 M.AutoInit();
 
 var toastMessage = document.querySelector('.message-container')
 var toastClose = document.querySelector('.toast-action') 
-M.toast({
-  html: toastMessage,
-  displayLength: 5000
-})
+if (toastMessage) {
+  M.toast({
+    html: toastMessage,
+    displayLength: 5000
+  })
+}
 
-toastClose.addEventListener('click', () => {
-  var toastElement = document.querySelector('.toast');
-  var toastInstance = M.Toast.getInstance(toastElement);
-  toastInstance.dismiss();
-})
+if (toastClose) {
+  toastClose.addEventListener('click', () => {
+    var toastElement = document.querySelector('.toast');
+    var toastInstance = M.Toast.getInstance(toastElement);
+    toastInstance.dismiss();
+  })
+}
+
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
@@ -19,3 +26,17 @@ document.addEventListener('DOMContentLoaded', function() {
     var modalElems = document.querySelectorAll('.modal');
     var modalInstances = M.Modal.init(modalElems, options);
   });
+
+  // Gallery mouse move
+
+  const galleryContainer = document.querySelector('.home-gallery')
+  const galleryImages = document.querySelector('.gallery-images-section')
+
+  galleryContainer.addEventListener('mousemove', (e)=> {
+    let x = e.clientX - galleryContainer.getBoundingClientRect().left
+    // let y = e.clientY - galleryContainer.getBoundingClientRect().top
+    console.log(x);
+
+    galleryImages.style.transform = `translate(-${x}px)`
+
+  })
