97a2ff818f19bf21853e8f702d08177d2a5c3a84
jjpickering10 <jjpickering10@live.com>
2021-11-30 11:06:55 +0000

    Add GSAP library and initial basic loading animation

--- static/js/script.js
+++ static/js/script.js
@@ -19,7 +19,6 @@ if (toastClose) {
   })
 }
 
-
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
@@ -38,5 +37,29 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log(x);
 
     galleryImages.style.transform = `translate(-${x}px)`
-
   })
+
+  // Loading
+
+  const documentElements = [document.querySelector('.main-section'), document.querySelector('.header-about-section')]
+
+  const timeline = gsap.timeline({defaults: {duration: 1}})
+
+  window.addEventListener('load', () => {
+    timeline.to(document.querySelector('.loading'), {
+      opacity: 0,
+      
+      ease: "circ.out"
+    }, '-=0.5').to(documentElements[0], {
+      opacity: 1,
+      y: '0',
+      ease: "power1.out"
+      
+    }, '-=1').to(documentElements[1], {
+      opacity: 1,
+      x: '0',
+      ease: "bounce.out",
+      
+    })
+    document.body.classList.remove('post-load')
+    })
