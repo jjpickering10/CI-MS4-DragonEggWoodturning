7604fabe84b7d6628c94877478c622125b1a7e94
jjpickering10 <jjpickering10@live.com>
2021-12-03 19:13:05 +0000

    Style contact page form

--- home/forms.py
+++ home/forms.py
@@ -8,4 +8,4 @@ class ContactForm(forms.Form):
     name = forms.CharField(required=True)
     email = forms.EmailField(required=True)
     subject = forms.CharField(required=True)
-    message = forms.CharField(required=True)
+    message = forms.CharField(required=True, widget=forms.Textarea)
